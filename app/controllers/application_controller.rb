1f10ea4001db550f0a078f657fba39d369402e59
liz539z <abc11nu@gmail.com>
2020-10-22 14:49:16 +0900

    applicationコントローラー編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :country_id, :birthday, :profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :country_id, :birthday, :profile, %i(avatar)])
   end
+
 end
